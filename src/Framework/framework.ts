77ae9339e1faae86afcf453ff8ffd8c8a98465c2
omron-tammar1 <mark.tammadge@omron.com>
2022-08-11 12:01:16 +0100

    Highlight selected button

--- src/Framework/framework.ts
+++ src/Framework/framework.ts
@@ -16,15 +16,26 @@ class framework {
 
         let header = document.querySelector('.header')!
         let main = document.querySelector('.main')!
+        let buttons: Array<HTMLButtonElement> = new Array<HTMLButtonElement>()
 
+        let index = 0
         this.components.forEach(c => {
             let button = document.createElement('button')
+            if (index === 0) {
+                button.classList.add('toolbarbuttonselected')
+                index++
+            }
             button.classList.add('toolbarbutton')
             button.innerText = c.name
             header.append(button)
-            button.addEventListener('click', () => {
+            buttons.push(button)
+
+            button.addEventListener('click', (e: MouseEvent) => {
                 removeChildren(main)
                 main.appendChild(c.render())
+                buttons.forEach(b => b.classList.remove('toolbarbuttonselected'))
+                let butt = (e.target as HTMLButtonElement)
+                butt.classList.add('toolbarbuttonselected')
             })
         })
 
