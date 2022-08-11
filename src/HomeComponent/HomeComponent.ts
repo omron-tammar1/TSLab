6067dae4555fa9d46d8fada52f31ca45d0928cc1
omron-tammar1 <mark.tammadge@omron.com>
2022-08-11 16:56:46 +0100

    Update for scrolling

--- src/HomeComponent/HomeComponent.ts
+++ src/HomeComponent/HomeComponent.ts
@@ -15,17 +15,17 @@ export class HomeComponent extends frameworkcomponent {
 
         let button = document.createElement('button')
         button.classList.add('homebutton')
-        button.innerText = 'ClickMe'
+        button.innerText = 'Lorem'
         div.append(button)
         button.addEventListener('click', () => {
-           alert("Hello World!")
-        })
+        //    alert("Hello World!")
 
-        for (let index = 0; index < 100; index++) {
-            let textdiv2 = document.createElement('div');
-            textdiv2.innerText = `${index} Hi`;
-            div.appendChild(textdiv2)          
-        }
+            for (let index = 0; index < 100; index++) {
+                let textdiv2 = document.createElement('div');
+                textdiv2.innerText = `${index} Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis atque cum est officia suscipit magni blanditiis. Amet accusamus impedit assumenda optio, quod delectus, in ab obcaecati eligendi et nam eaque.`
+                div.appendChild(textdiv2)          
+            }
+        })
 
         this.view = div;
     }
