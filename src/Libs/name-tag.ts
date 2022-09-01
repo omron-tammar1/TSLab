3af34c14414cd8109c0c98f230f1a9947f326fb2
omron-tammar1 <mark.tammadge@omron.com>
2022-09-01 08:42:16 +0100

    Added Time web component

--- src/Libs/name-tag.ts
+++ src/Libs/name-tag.ts
@@ -51,5 +51,83 @@ class UserCard extends HTMLElement {
     m.append(p);
     return m;
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+  // export interface DateTimeFormatOptions extends Intl.DateTimeFormatOptions {
+  //   localeMatcher?: 'best fit' | 'lookup';
+  //   weekday?: 'long' | 'short' | 'narrow';
+  //   era?:  'long' | 'short' | 'narrow';
+  //   year?: 'numeric' | '2-digit';
+  //   month?: 'numeric' | '2-digit' | 'long' | 'short' | 'narrow';
+  //   day?: 'numeric' | '2-digit';
+  //   hour?: 'numeric' | '2-digit';
+  //   minute?: 'numeric' | '2-digit';
+  //   second?: 'numeric' | '2-digit';
+  //   timeZoneName?: 'long' | 'short';
+  //   formatMatcher?: 'best fit' | 'basic';
+  //   hour12?: boolean;
+  //   /**
+  //    * Timezone string must be one of IANA. UTC is a universally required recognizable value
+  //    */
+  //   timeZone?: 'UTC' | string;
+  //   dateStyle?: 'full' | 'long' | 'medium' | 'short',
+  //   timeStyle?: 'full' | 'long' | 'medium' | 'short',
+  //   calendar?: 'buddhist' | 'chinese' | ' coptic' | 'ethiopia' | 'ethiopic' | 'gregory' | ' hebrew' | 'indian' | 'islamic' | 'iso8601' | ' japanese' | 'persian' | 'roc',
+  //   dayPeriod?: 'narrow' | 'short' | 'long',
+  //   numberingSystem?: 'arab' | 'arabext' | 'bali' | 'beng' | 'deva' | 'fullwide' | ' gujr' | 'guru' | 'hanidec' | 'khmr' | ' knda' | 'laoo' | 'latn' | 'limb' | 'mlym' | ' mong' | 'mymr' | 'orya' | 'tamldec' | ' telu' | 'thai' | 'tibt',
+  //   hourCycle?: 'h11' | 'h12' | 'h23' | 'h24',
+  //   /**
+  //    * Warning! Partial support 
+  //    */
+  //   fractionalSecondDigits?: 0 | 1 | 2 | 3
+  // }
+
+
+
+  class TimeFormatted extends HTMLElement {
+
+    connectedCallback() {
+      let date = new Date(this.getAttribute('datetime') || Date.now());
+
+    let options:Intl.DateTimeFormatOptions  = {
+        year: this.getAttribute('year') as 'numeric' | '2-digit',
+        month: this.getAttribute('month') as 'numeric' | '2-digit' | 'long' | 'short' | 'narrow',
+        day: this.getAttribute('day') as 'numeric' | '2-digit',
+        hour: this.getAttribute('hour') as 'numeric' | '2-digit',
+        minute: this.getAttribute('minute') as 'numeric' | '2-digit',
+        second: this.getAttribute('second') as 'numeric' | '2-digit',
+        timeZoneName: this.getAttribute('time-zone-name') as 'long' | 'short',
+      }
+
+      this.innerHTML = new Intl.DateTimeFormat("default", options
+      ).format(date);
+    }
+  
+  }
+  
+  customElements.define("time-format", TimeFormatted);
+
+
   
   export {init};
