a965e3fecd372d64cf3e9f74fdc9ba071e726155
omron-tammar1 <mark.tammadge@omron.com>
2022-09-07 17:10:34 +0100

    three

---
+++ src/KinematicComponent/KinematicComponent.ts
@@ -0,0 +1,33 @@
+import { frameworkcomponent } from '../Framework/frameworkcomponent';
+
+export class KinematicComponent extends frameworkcomponent {
+
+    constructor() {
+
+        super('Kinematic Demo');
+
+        // let canvas = document.createElement('canvas')
+        // canvas.setAttribute('id', 'app')
+
+
+        // let textdiv = document.createElement('div');
+        // textdiv.innerText = `${this.name} Override`;
+        // div.appendChild(textdiv)
+
+        // let button = document.createElement('button')
+        // button.classList.add('homebutton')
+        // button.innerText = 'Lorem'
+        // div.append(button)
+        // button.addEventListener('click', () => {
+        // //    alert("Hello World!")
+
+        //     for (let index = 0; index < 100; index++) {
+        //         let textdiv2 = document.createElement('div');
+        //         textdiv2.innerText = `${index} Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis atque cum est officia suscipit magni blanditiis. Amet accusamus impedit assumenda optio, quod delectus, in ab obcaecati eligendi et nam eaque.`
+        //         div.appendChild(textdiv2)          
+        //     }
+        // })
+
+        //this.view = canvas;
+    }
+}
--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,9 @@ import { frameworkcomponent } from './Framework/frameworkcomponent';
 import { framework } from './Framework/framework';
 import "./main.css";
 import { HomeComponent } from './HomeComponent/HomeComponent';
-import { KinematicComponent } from './KinematicComponent';
+import { KinematicComponent } from './KinematicComponent/KinematicComponent';
 import { init2 } from './Libs/omron-tag';
+import * as THREE from 'three'
 
 let frame = new framework()
 frame.addComponent(new HomeComponent())
@@ -23,4 +24,30 @@ init2()
 
 // document.body.appendChild(component());
 // document.body.appendChild(CreateName('Test, injected text'));
+
+const width = 300 // window.innerWidth
+const height = 300 // window.innerHeight
+
+const renerer = new THREE.WebGLRenderer({
+    canvas: document.getElementById('app') as HTMLCanvasElement
+})
+renerer.setSize(width, height)
+const mainCamera = new THREE.PerspectiveCamera(60, width/height, 0.1, 100)
+
+const scene = new THREE.Scene()
+const geometry = new THREE.BoxGeometry()
+const material = new THREE.MeshPhongMaterial({color: 0xFFAD00})
+const cube = new THREE.Mesh(geometry, material)
+cube.position.z = -5
+cube.position.y = -1
+scene.add(cube)
+
+const light = new THREE.DirectionalLight(0xFFFFFF, 1)
+light.position.set(0,4,2)
+scene.add(light)
+
+renerer.render(scene, mainCamera)
+
+
+
 
