d211a03ed544d997265cc6ef58f174aa8e690b1b
omron-tammar1 <mark.tammadge@omron.com>
2022-09-09 15:16:04 +0100

    Moved lorem text to kinematic

--- src/HomeComponent/HomeComponent.ts
+++ src/HomeComponent/HomeComponent.ts
@@ -13,20 +13,6 @@ export class HomeComponent extends frameworkcomponent {
         textdiv.innerText = `${this.name} Override`;
         div.appendChild(textdiv)
 
-        let button = document.createElement('button')
-        button.classList.add('homebutton')
-        button.innerText = 'Lorem'
-        div.append(button)
-        button.addEventListener('click', () => {
-        //    alert("Hello World!")
-
-            for (let index = 0; index < 100; index++) {
-                let textdiv2 = document.createElement('div');
-                textdiv2.innerText = `${index} Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis atque cum est officia suscipit magni blanditiis. Amet accusamus impedit assumenda optio, quod delectus, in ab obcaecati eligendi et nam eaque.`
-                div.appendChild(textdiv2)          
-            }
-        })
-
         this.view = div;
     }
 }
